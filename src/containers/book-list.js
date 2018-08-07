290bec0fe88da8814dcb54a983e91ea8c6dbccf3
arya.vijayan <arya.vijayan@experionglobal.com>
2018-08-07 15:24:48 +0530

    books added

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -28,22 +28,13 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
-  // Whatever is returned will show up as props
-  // inside of BookList
   return {
     books: state.books
   };
 }
 
-// Anything returned from this function will end up as props
-// on the BookList container
 function mapDispatchToProps(dispatch) {
-  // Whenever selectBook is called, the result shoudl be passed
-  // to all of our reducers
   return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
-// Promote BookList from a component to a container - it needs to know
-// about this new dispatch method, selectBook. Make it available
-// as a prop.
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,4 @@
-// State argument is not application state, only the state
-// this reducer is responsible for
+
 export default function(state = null, action) {
   switch (action.type) {
     case "BOOK_SELECTED":
